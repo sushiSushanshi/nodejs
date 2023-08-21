400e9c29c3be7e986fcff5a9174f339bbf4a503b
sushiSushanshi <50866965+sushiSushanshi@users.noreply.github.com>
2023-08-21 14:49:45 +0530

    Delete react directory
    unnecessary file

--- react/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "font-awesome/css/font-awesome.min.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
